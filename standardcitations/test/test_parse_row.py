6fa110fac6025f58a8f52164296a7ee44c279bd6
Martin Isaksson <martin.isaksson@gmail.com>
2019-02-10 19:45:16 +0100

    Test: Add testcase for the parse_row function
    Test the parse row function by reading the `test_input.xlsx` workbook
    and reading the first (real) row.

---
+++ standardcitations/test/test_parse_row.py
@@ -0,0 +1,23 @@
+"""
+This test module contains basic tests for the parse_row function
+in standardcitations.
+"""
+
+from standardcitations import standardcitations
+
+
+def test_parse_row():
+    """
+    Test the parse row function by reading the `test_input.xlsx` workbook
+    and reading the first (real) row.
+    """
+
+    ws = standardcitations.get_workbook(
+        "standardcitations/test/test_input.xlsx")
+
+    number, title, doctype = standardcitations.parse_row(ws.__getitem__('2'))
+
+    assert number == u'36.101'
+    assert title == u'Evolved Universal Terrestrial Radio Access ' \
+        '(E-UTRA); User Equipment (UE) radio transmission and reception'
+    assert doctype == u'Technical Specification (TS)'
