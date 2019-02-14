b6ae796fade3b2053df7a9f855703825111ffe4c
Martin Isaksson <martin.isaksson@gmail.com>
2019-02-14 09:49:33 +0100

    Enhancement: Add Trove classification
    Add Trove classification according to PEP-301
    (https://www.python.org/dev/peps/pep-0301/#distutils-trove-classification).

--- setup.py
+++ setup.py
@@ -25,6 +25,27 @@ setup(
     author_email='martin.isaksson@gmail.com',
     name='3gpp-citations',
     version='1.1.0',
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        # Indicate who your project is intended for
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering',
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Natural Language :: English',
+    ],
     url='https://github.com/martisak/3gpp-citations',
     packages=['standardcitations', ],
     license=open('LICENSE').read(),
